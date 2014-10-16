7aa1ebf04b9c36d424cbb9ffb9b46eb8af5de25d
carlodeguzman5 <carlodeguzman5@gmail.com>
2014-10-16 12:58:48 +0800

    Added Android Server Files

---
+++ Android Server/Server.php
@@ -0,0 +1,19 @@
+<?php
+$con=mysqli_connect("localhost","root","","greenpigsdb");
+if (mysqli_connect_errno($con))
+{
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$username = $_POST['username'];
+$password = $_POST['password'];
+$result = mysqli_query($con,"SELECT * FROM login where 
+username='$username' and password='$password'");
+$data = 'Allow';
+if (mysqli_num_rows ($result) != 0){
+	echo $data;
+}
+else{
+	echo 'Disallow';
+}
+mysqli_close($con);
+?>
